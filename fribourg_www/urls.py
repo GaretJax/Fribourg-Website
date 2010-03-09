a8cc178b2b214b8edd7aebe019a8efbea3afe4b9
Jonathan Stoppani <garetjax@Jonathan-Stoppanis-MacBook-Pro.local>
2010-03-09 16:54:50 +0100

    Corrected a bug in the development versione which prevented the media to load

--- fribourg_www/urls.py
+++ fribourg_www/urls.py
@@ -7,10 +7,13 @@ urlpatterns = patterns('',
     #url(r'^$', 'coffin.views.generic.simple.direct_to_template', {'template': 'index.html'}, name='index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('cms.urls')),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^%s(?P<path>.*)$' % (settings.MEDIA_URL[1:]), 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
+
+urlpatterns += patterns('',
+    url(r'^', include('cms.urls')),
+)
